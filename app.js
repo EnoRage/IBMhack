ce289b5156c2ac611bfb3dfedc115f505eae24ba
Kirill <kk@bankexfoundation.org>
2018-04-27 12:26:46 +0300

    fix

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 
 const Server = require("./server.js"),
     db = require("./db.js"),
+    builder = require('botbuilder'),
     rp = require('request-promise');
 
 
 var bot = new builder.UniversalBot(Server.connector, [
     function (session) {
-        console.log('lol')
         session.beginDialog('start');
     }
 ]).set('storage', Server.memory);
